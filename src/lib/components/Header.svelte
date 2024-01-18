6a6214b11f01d9abc65c4cd5d8ea5221b92a5efb
David Pustoslemšek <pusti@Dejv.>
2024-01-18 09:46:58 +0100

    Profile components in navbar

--- src/lib/components/Header.svelte
+++ src/lib/components/Header.svelte
@@ -1,8 +1,10 @@
 <script lang="ts">
+	import * as DropdownMenu from '$lib/components/ui/dropdown-menu';
 	import { authData } from '$lib/stores/auth';
 	import toast from 'svelte-french-toast';
 	import Profile from './Profile.svelte';
 	import Button from './ui/button/button.svelte';
+	import DropdownMenuItem from './ui/dropdown-menu/dropdown-menu-item.svelte';
 
 	async function logout() {
 		await fetch('/api/logout', { method: 'POST' });
@@ -12,21 +14,33 @@
 </script>
 
 <header
-	class="page-paddings fixed inset-0 z-50 flex h-nav items-center border-b-2 bg-secondary font-bold"
+	class="page-paddings fixed inset-0 z-50 flex h-nav items-center border-b-2 bg-white font-bold"
 >
 	<nav class="mx-auto flex w-full items-center justify-between">
 		<a href="/">Pondelk</a>
 		<ul class="flex items-center gap-3">
 			{#if $authData}
 				<li>
-					<Button variant="outline" on:click={logout}>Logout</Button>
-				</li>
-				<li>
-					<Profile username={$authData.username} avatar={$authData.picture ?? ''} />
+					<DropdownMenu.Root>
+						<DropdownMenu.Trigger asChild let:builder>
+							<Button class="h-11" variant="outline" builders={[builder]}>
+								<Profile
+									withUsername
+									username={$authData.username}
+									avatar={$authData.picture ?? ''}
+								/>
+							</Button>
+						</DropdownMenu.Trigger>
+						<DropdownMenu.Content class="w-44">
+							<DropdownMenu.Group class="[&_div]:cursor-pointer">
+								<DropdownMenuItem on:click={logout}>Logout</DropdownMenuItem>
+							</DropdownMenu.Group>
+						</DropdownMenu.Content>
+					</DropdownMenu.Root>
 				</li>
 			{:else}
 				<li>
-					<a href="/signup">Sign Up</a>
+					<Button variant="outline" href="/signup">Sign Up</Button>
 				</li>
 			{/if}
 		</ul>
--- src/lib/components/Profile.svelte
+++ src/lib/components/Profile.svelte
@@ -1,8 +1,17 @@
 <script lang="ts">
 	import * as Avatar from '$lib/components/ui/avatar';
 
+	const sizes = {
+		sm: 'h-9 w-9',
+		md: 'h-10 w-10',
+		lg: 'h-12 w-12'
+	};
+
+	export let size: keyof typeof sizes = 'sm';
 	export let username: string;
 	export let avatar: string;
+	export let withUsername = false;
+	export let reverse = true;
 
 	$: initials = username?.split('')[0];
 
@@ -38,16 +47,21 @@
 	$: color = pickRandomColor();
 </script>
 
-<Avatar.Root>
-	{#if avatar}
-		<Avatar.Image src={avatar} alt="avatar" />
-	{:else}
-		<Avatar.Fallback
-			style="
-			background-color: {color.background};
-			border-color: {color.border};
-		"
-			class={`border-2 text-white`}>{initials}</Avatar.Fallback
-		>
+<div class={`flex items-center gap-2 ${reverse && 'flex-row-reverse'}`}>
+	<Avatar.Root class={sizes[size]}>
+		{#if avatar}
+			<Avatar.Image src={avatar} alt="avatar" />
+		{:else}
+			<Avatar.Fallback
+				style="
+				background-color: {color.background};
+				border-color: {color.border};
+			"
+				class={`border-2 text-white`}>{initials}</Avatar.Fallback
+			>
+		{/if}
+	</Avatar.Root>
+	{#if withUsername}
+		<span>{username}</span>
 	{/if}
-</Avatar.Root>
+</div>
