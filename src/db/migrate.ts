d213e2710c887177bc6b7304c96fcd7b556daaeb
DavidPugg <pusti.david@gmail.com>
2024-02-20 17:26:51 +0100

    add retry for migrations

--- src/db/migrate.ts
+++ src/db/migrate.ts
@@ -4,4 +4,21 @@ import postgres from 'postgres';
 import { dbConnection } from './db';
 
 const migrationClient = postgres(dbConnection);
-migrate(drizzle(migrationClient), { migrationsFolder: 'drizzle' });
+
+(async () => {
+	for (let i = 0; i < 3; i++) {
+		try {
+			await migrate(drizzle(migrationClient), { migrationsFolder: 'drizzle' });
+			break;
+		} catch (e) {
+			i++;
+			console.log('Error migrating to database, Error: ', e);
+		}
+
+		if (i === 5) {
+			console.error('Migration failed after 5 attempts');
+		}
+
+		await new Promise((resolve) => setTimeout(resolve, 1000));
+	}
+})();
